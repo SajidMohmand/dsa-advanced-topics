de32133c7006cb1ebe14d354729291831bc4ba54
SajidMohmand <sajid.muhmand99@gmail.com>
2025-09-22 19:05:14 +0500

    add BFS code and adjacency list of graph

--- Graph/GraphAlgorithms.java
+++ Graph/GraphAlgorithms.java
@@ -1,6 +1,8 @@
 package Graph;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class GraphAlgorithms {
 
@@ -14,29 +16,62 @@ public class GraphAlgorithms {
         }
     }
 
-    public static void createGraph(ArrayList<Edge> graph[]){
+    public static void createGraph(ArrayList<Edge>[] graph){
 
         for (int i=0; i<graph.length; i++){
             graph[i] = new ArrayList<Edge>();
         }
 
+        graph[0].add(new Edge(0,1));
         graph[0].add(new Edge(0,2));
 
-        graph[1].add(new Edge(1,2));
+        graph[1].add(new Edge(1,0));
         graph[1].add(new Edge(1,3));
 
         graph[2].add(new Edge(2,0));
-        graph[2].add(new Edge(2,1));
-        graph[2].add(new Edge(2,3));
+        graph[2].add(new Edge(2,4));
 
         graph[3].add(new Edge(3,1));
-        graph[3].add(new Edge(3,2));
+        graph[3].add(new Edge(3,4));
+        graph[3].add(new Edge(3,5));
+
+        graph[4].add(new Edge(4,2));
+        graph[4].add(new Edge(4,3));
+        graph[4].add(new Edge(4,5));
+
+        graph[5].add(new Edge(5,3));
+        graph[5].add(new Edge(5,4));
+        graph[5].add(new Edge(5,6));
+
+        graph[6].add(new Edge(6,5));
+
 
     }
 
+
+    // time complexity: O(V+E)
+    public static void bfs(ArrayList<Edge>[] graph, boolean[] vis, int x){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(x);
+        while (!q.isEmpty()){
+            int curr = q.poll();
+            if (!vis[curr]){
+                System.out.print(curr+" ");
+                vis[curr] = true;
+
+                for (int i=0; i<graph[curr].size(); i++){
+                    q.add(graph[curr].get(i).dest);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        int v = 4;
-        ArrayList<Edge> graph[] = new ArrayList[4];
+        int v = 7;
+        ArrayList<Edge> graph[] = new ArrayList[v];
+
+        createGraph(graph);
+        bfs(graph,new boolean[v],0);
 
     }
 }
