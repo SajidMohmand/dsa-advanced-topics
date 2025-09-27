ddaa5430e38fd7de15c8e8817490382ea890ad03
SajidMohmand <sajid.muhmand99@gmail.com>
2025-09-27 15:01:05 +0500

    graph folder updated with new question

--- Graph/GraphAlgorithms.java
+++ Graph/GraphAlgorithms.java
@@ -22,28 +22,38 @@ public class GraphAlgorithms {
             graph[i] = new ArrayList<Edge>();
         }
 
-        graph[0].add(new Edge(0,1));
+        // undirected graph
+//        graph[0].add(new Edge(0,1));
+//        graph[0].add(new Edge(0,2));
+//
+//        graph[1].add(new Edge(1,0));
+//        graph[1].add(new Edge(1,3));
+//
+//        graph[2].add(new Edge(2,0));
+//        graph[2].add(new Edge(2,4));
+//
+//        graph[3].add(new Edge(3,1));
+//        graph[3].add(new Edge(3,4));
+//        graph[3].add(new Edge(3,5));
+//
+//        graph[4].add(new Edge(4,2));
+//        graph[4].add(new Edge(4,3));
+//        graph[4].add(new Edge(4,5));
+//
+//        graph[5].add(new Edge(5,3));
+//        graph[5].add(new Edge(5,4));
+//        graph[5].add(new Edge(5,6));
+//
+//        graph[6].add(new Edge(6,5));
+
+        // directed graph
         graph[0].add(new Edge(0,2));
 
         graph[1].add(new Edge(1,0));
-        graph[1].add(new Edge(1,3));
 
-        graph[2].add(new Edge(2,0));
-        graph[2].add(new Edge(2,4));
+        graph[2].add(new Edge(2,3));
 
-        graph[3].add(new Edge(3,1));
-        graph[3].add(new Edge(3,4));
-        graph[3].add(new Edge(3,5));
-
-        graph[4].add(new Edge(4,2));
-        graph[4].add(new Edge(4,3));
-        graph[4].add(new Edge(4,5));
-
-        graph[5].add(new Edge(5,3));
-        graph[5].add(new Edge(5,4));
-        graph[5].add(new Edge(5,6));
-
-        graph[6].add(new Edge(6,5));
+        graph[3].add(new Edge(3,0));
 
 
     }
@@ -85,23 +95,41 @@ public class GraphAlgorithms {
     // time complexity: O(V^V)
     public static void allPathFromRange(ArrayList<Edge>[] graph,int curr,boolean[] vis,String path,int target){
         if (curr == target){
-            path+=curr;
             System.out.println(path);
             return;
         }
 
-        path += curr;
-        vis[curr] = true;
         for (int i=0; i<graph[curr].size(); i++){
             int val = graph[curr].get(i).dest;
             if (!vis[val]){
-                allPathFromRange(graph,val,vis,path,target);
-                vis[val] = false;
+                vis[curr] = true;
+                allPathFromRange(graph,val,vis,path+val,target);
+                vis[curr] = false;
             }
         }
     }
+
+    // 2:Cycle detection
+    // time complexity: O(E+V)
+    public static boolean cycleDetection(ArrayList<Edge>[] graph,boolean[] vis,int curr,boolean[] stackArr){
+        vis[curr] = true;
+        stackArr[curr] = true;
+        for (int i=0; i<graph[curr].size(); i++){
+            int val = graph[curr].get(i).dest;
+
+            if (stackArr[val]){
+                return true;
+            }else if (!vis[val]){
+                if (cycleDetection(graph, vis, val, stackArr)) {
+                    return true;
+                }
+            }
+        }
+        stackArr[curr] = false;
+        return false;
+    }
     public static void main(String[] args) {
-        int v = 7;
+        int v = 4;
         ArrayList<Edge> graph[] = new ArrayList[v];
 
         createGraph(graph);
@@ -116,7 +144,16 @@ public class GraphAlgorithms {
 //        }
 
 //        dfs(graph,new boolean[v],0);
-        allPathFromRange(graph,0,new boolean[v],"",5);
+//        allPathFromRange(graph,0,new boolean[v],"0",5);
 
+        boolean[] stackArr = new boolean[v];
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]) {
+                if (cycleDetection(graph, vis, i, stackArr)) {
+                    System.out.println("Cycle found");
+                    break;
+                }
+            }
+        }
     }
 }
